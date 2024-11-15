f3b258a4190d19170104c9483d9b3542825552f9
KhilfiKhairulAmin <khilfikhairulamin@gmail.com>
2024-11-15 10:18:44 +0800

    Set up basic blueprint for Publisher.h

--- Publisher.h
+++ Publisher.h
@@ -2,30 +2,45 @@
 #define PUBLISHER_H
 #include <string>
 #include <ctime>
+#include <vector>
 using namespace std;
 
-
-class Publisher
+struct Publisher
 {
-  private:
-    int id;  // Unique identifier for the publisher
-    string email;  // Will be used for sign up and log in
-    string password;  // Used for authentication
-    string name;  // Publicly shown name
-    time_t timeRegistered;  // Stored in time after epoch format (seconds)
-    
-    static int nextId;  // 
-  public:
-    Publisher(int, string, string, string, time_t);  // This one is default to load Publisher from file
-    Publisher(string, string, string);  // This one is used when creating new Publisher
-    Publisher();
-    void displayInfo();
-    int getId();
-    string getEmail();
-    string getPassword();
-    string getName();
-    time_t getTimeRegistered();
+  int id;
+  string email;
+  string password;
+  string name;
+  string about;
+  string phone;
 };
 
+class PublisherManager
+{
+  protected:
+    /** Vector containing `Publishers`. */
+    vector<Publisher> publishers;
+    /** Keeps total number of publishers */
+    static int totalPublishers;
+    /** Tracks next unique value for `Publisher.id` */
+    static int nextId;
+  public:
+    /** Returns a constant vector containing all `Publishers`. */
+    const vector<Publisher>& getPublishers() const;
+    /** Returns one `Publisher` based on given `id`. */
+    const Publisher& getOnePublisher(int id) const;
+    /** Creates a new `Publisher`, then returns its constant pointer.  */
+    const Publisher& createPublisher(string email, string password, string name, string phone, string about) const;
+    /** Modifies data inside a `Publisher`, then return its constant pointer. */
+    const Publisher& editPublisher(int id, string newEmail = "", string newPassword = "", string newName = "", string newPhone = "", string about = "") const;
+    /** Deletes a `Publisher`. */
+    void deletePublisher(int id) const;
+    /** Lazily searches names of `Publishers`, then return a constant vector. */
+    const vector<Publisher>& searchName(string name);
+    /** Lazily searches phone numbers of `Publishers`, then return a constant vector. */
+    const vector<Publisher>& searchPhone(string phone);
+    /** Returns a constant vector containing all `Publishers` sorted ascendingly/descendingly based on `Publisher.name` attribute. */
+    const vector<Publisher>& sortPublishers(bool ascending = true);
+};
 
 #endif
