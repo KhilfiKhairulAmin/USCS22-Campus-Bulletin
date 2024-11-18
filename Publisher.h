d9c80dcbf0589a58cfeba9c78c1e56fab8ba8a3f
KhilfiKhairulAmin <khilfikhairulamin@gmail.com>
2024-11-18 08:40:13 +0800

    Removed const from definitions

--- Publisher.h
+++ Publisher.h
@@ -25,24 +25,27 @@ class PublisherManager
     /** Tracks next unique value for `Publisher.id` */
     static int nextId;
   public:
-    /** Returns a constant vector containing all `Publishers`. */
-    const vector<Publisher>& getPublishers() const;
+    /** Loads `Publishers` from database. */
+    PublisherManager();
+    /** Returns a vector containing all `Publishers`. */
+    vector<Publisher> getPublishers();
     /** Creates a new `Publisher`, then returns its constant pointer.  */
-    const Publisher& createPublisher(string email, string password, string name, string phone, string about) const;
+    Publisher createPublisher(string email, string password, string name, string phone, string about);
     /** Modifies data inside a `Publisher`, then return its constant pointer. */
-    const Publisher& editPublisher(int id, string newEmail = "", string newPassword = "", string newName = "", string newPhone = "", string about = "") const;
+    Publisher editPublisher(int id, string newEmail = "", string newPassword = "", string newName = "", string newPhone = "", string about = "");
     /** Deletes a `Publisher`. */
-    void deletePublisher(int id) const;
+    void deletePublisher(int id);
     /** Returns one `Publisher` based on given `id`. */
-    const Publisher& searchId(int id) const;
+    Publisher searchId(int id);
     /** Returns one `Publisher` based on given `email`. */
-    const Publisher& searchEmail(string email) const;
+    Publisher searchEmail(string email);
     /** Lazily searches names of `Publishers`, then return a constant vector. */
-    const vector<Publisher>& searchName(string name);
+    vector<Publisher>& searchName(string name);
     /** Lazily searches phone numbers of `Publishers`, then return a constant vector. */
-    const vector<Publisher>& searchPhone(string phone);
+    vector<Publisher>& searchPhone(string phone);
     /** Returns a constant vector containing all `Publishers` sorted ascendingly/descendingly based on `Publisher.name` attribute. */
-    const vector<Publisher>& sortPublishers(bool ascending = true);
+    vector<Publisher>& sortPublishers(bool ascending = true);
+    
 };
 
 #endif
