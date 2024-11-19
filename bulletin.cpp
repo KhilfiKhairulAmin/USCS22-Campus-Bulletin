c906f9e255cedbb06d097b2c8df2ddde528ab2a3
KhilfiKhairulAmin <khilfikhairulamin@gmail.com>
2024-11-19 21:04:11 +0800

    d

--- bulletin.cpp
+++ bulletin.cpp
@@ -1,18 +1,68 @@
-#include <Publisher.h>
-#include <News.h>
+#include "Publisher.h"
+#include "News.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
 using namespace std;
 
-const vector<Publisher>& PublisherManager::getPublishers() const
+int PublisherManager::totalPublishers = 0;
+int PublisherManager::nextId = 1;
+
+PublisherManager::PublisherManager()
+{
+  ifstream file("database/publishers.txt");
+  
+  if (!file.is_open())
+  {
+    throw "Failed to open database/publishers.txt";
+  }
+  
+  string line;
+  getline(file, line);  // Skip first line as it is header of the file
+  while (getline(file, line))
+  {
+    string id, email, password, name, about, phone;
+    stringstream ss(line);
+    vector<string> fields;
+
+    // Split by ^ (the delimiter)
+    while (getline(ss, id, '^'))
+    {
+      fields.push_back(id);
+    }
+    
+    if (fields.size() == 6)
+    {
+      publishers.push_back(Publisher
+      {
+        stoi(fields[0]),
+        fields[1],
+        fields[2], 
+        fields[3],
+        fields[4],
+        fields[5]
+      });
+    }
+
+    totalPublishers++;
+    nextId++;
+  }
+}
+
+vector<Publisher> PublisherManager::getPublishers()
 {
   return publishers;
 }
 
-const Publisher& PublisherManager::createPublisher(string email, string password, string name, string phone, string about) const
+Publisher PublisherManager::createPublisher(string email, string password, string name, string phone, string about)
 {
-    this->
+  publishers.push_back(Publisher{nextId, email, password, name, phone, about});
+  totalPublishers++;
+  nextId++;
+  return publishers.back();
 }
 
-const Publisher& PublisherManager::searchId(int id) const
+Publisher PublisherManager::searchId(int id)
 { 
   for (auto it = publishers.begin(); it < publishers.end(); it++)
   {
@@ -24,7 +74,7 @@ const Publisher& PublisherManager::searchId(int id) const
   throw "ID doesn't exist";
 }
 
-const Publisher& PublisherManager::searchEmail(string email) const
+Publisher PublisherManager::searchEmail(string email)
 {
   for (auto it = publishers.begin(); it < publishers.end(); it++)
   {
@@ -34,4 +84,25 @@ const Publisher& PublisherManager::searchEmail(string email) const
     }
   }
   throw "Email doesn't exist";
+}
+
+int main()
+{
+  PublisherManager publisherManager = PublisherManager();
+  vector<Publisher> publishers = publisherManager.getPublishers();
+  publishers[0].about = "Hello World";
+  for (auto it = publishers.begin(); it < publishers.end(); it++)
+  {
+    cout << it->id << " " << it->email << " " << it->password << " " << it->name << " " << it->phone << " " << it->about << endl;
+  }
+  vector<Publisher> publishers2 = publisherManager.getPublishers();
+  publishers2[0].name = "Hello World";
+    for (auto it = publishers2.begin(); it < publishers2.end(); it++)
+  {
+    cout << it->id << " " << it->email << " " << it->password << " " << it->name << " " << it->phone << " " << it->about << endl;
+  }
+    for (auto it = publishers.begin(); it < publishers.end(); it++)
+  {
+    cout << it->id << " " << it->email << " " << it->password << " " << it->name << " " << it->phone << " " << it->about << endl;
+  }
 }
