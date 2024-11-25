74ae3702f890702705d8b231963b481bd12eb11f
KhilfiKhairulAmin <khilfikhairulamin@gmail.com>
2024-11-25 20:17:07 +0800

    Refactored all codes into its corresponding header files

--- headers/Database.h
+++ headers/Database.h
@@ -14,31 +14,104 @@ class Database: public NewsManager, public PublisherManager
     
   public:
     /** Loads `Publishers` and `News` from database. */
-    Database();
+    Database::Database(): NewsManager(), PublisherManager() {};
 
     /** Get all `News` published by a `Publisher` with specified `id`. */
-    vector<int> getNewsByPublisher(int publisherId) const;
+    vector<int> Database::getNewsByPublisher(int publisherId) const
+    {
+      int cur = 0;
+      vector<int> indexes;
+      for (auto it = news->begin(); it < news->end();it++)
+      {
+        if (it->publisherId == publisherId)
+          indexes.push_back(cur);
+        cur++;
+      }
+      return indexes;
+    }
 
     /** Get total likes from the `Publisher` with specified `id`. */
-    int getTotalLikes(int publisherId) const;
+    int Database::getTotalLikes(int publisherId) const
+    {
+      int total = 0;
+      for (auto it = news->begin(); it < news->end();it++)
+        if (it->publisherId == publisherId)
+          total += it->numOfLikes;
+      return total;
+    }
 
     /** Get total dislikes from the `Publisher` with specified `id`. */
-    int getTotalDislikes(int publisherId) const;
+    int Database::getTotalDislikes(int publisherId) const
+    {
+      int total = 0;
+      for (auto it = news->begin(); it < news->end();it++)
+        if (it->publisherId == publisherId)
+          total += it->numOfDislikes;
+      return total;
+    }
 
     /** Get total `News` published by `Publisher` with specified `id`.  */
-    int getTotalNewsPublished(int publisherId) const;
+    int Database::getTotalNewsPublished(int publisherId) const
+    {
+      int total = 0;
+      for (auto it = news->begin(); it < news->end();it++)
+        if (it->publisherId == publisherId)
+          total++;
+      return total;
+    }
 
     /** Get total `Publishers` registered inside the database. */
-    int getTotalPublishers() const;
+    int Database::getTotalPublishers() const
+    {
+      return totalPublishers;
+    }
 
     /** Get total `News` created inside the database. */
-    int getTotalNews() const;
+    int Database::getTotalNews() const
+    {
+      return totalPublishers;
+    }
 
     /** Get popularity rank of `Publishers`. */
-    vector<array<int, 2>> getPopularityRankings() const;
+    vector<array<int, 2>> Database::getPopularityRankings() const
+    {
+      struct IndexSum
+      {
+        int i = -1;
+        int sum = 0;
+
+        bool operator<(IndexSum is)
+        {
+          return sum < is.sum;
+        }
+      };
+
+      unordered_map<int, IndexSum> m;
+      int i = 0;
+      for (auto it = publishers->begin(); it < publishers->end(); it++)
+        m.insert(make_pair( it->id, IndexSum{ i++, 0 }));
+
+      int j = 0;
+      for (auto it = news->begin(); it < news->end(); it++)
+        m[it->publisherId].sum += it->numOfLikes - it->numOfDislikes;
+      
+      vector<IndexSum> r;
+      for (auto it = publishers->begin(); it < publishers->end(); it++)
+        r.push_back(m[it->id]);
+
+      sort(r.rbegin(), r.rend());
+
+      vector<array<int, 2>> ids;
+      for (auto it = r.begin(); it < r.end(); it++)
+      {
+        ids.push_back({ it->i, it->sum });
+      }
+      
+      return ids;
+    }
 
     /** Writes `Publishers` and `News` into database and deletes DMA inside the program. */
-    ~Database();
+    Database::~Database() {};
 };
 
 
--- headers/News.h
+++ headers/News.h
@@ -1,8 +1,9 @@
 #ifndef NEWS_H
 #define NEWS_H
+#include "Datetime.h"
+#include "Utility.h"
 #include <string>
 #include <vector>
-#include "Datetime.h"
 
 struct News
 {
@@ -34,34 +35,158 @@ class NewsManager
 
   public:
     /** Returns a constant vector containing all `News`. */
-    const vector<News>* getAllNews() const;
+    const vector<News>* NewsManager::getAllNews() const
+    {
+      return news;
+    }
 
     /** Creates a `News`, then return the index of the `News`.  */
-    int createNews(int publisherId, string title, string content);
+    int NewsManager::createNews(int publisherId, string title, string content)
+    {
+      Datetime now = Datetime::getCurrentDatetime();
+      news->push_back(News {
+        nextNewsId,
+        publisherId,
+        now,
+        now,
+        Datetime(),
+        title,
+        content,
+        0,
+        0,
+        false
+      });
+      totalNews++;
+      nextNewsId++;
+      return totalNews-1;
+    }
 
     /** Modifies data inside a `News` with specified `id`. */
-    void editNews(int id, string newTitle, string newContent);
+    void NewsManager::editNews(int id, string newTitle = "", string newContent = "")
+    {
+      int index = searchNewsId(id);
+      Datetime now = Datetime::getCurrentDatetime();
+      News* n = &(news->at(index));
+      if (newTitle != "")
+        n->title = newTitle;
+      if (newContent != "")
+        n->content = newContent;
+    }
 
     /** Deletes a `News` with specified `id`. */
-    void deleteNews(int id);
+    void NewsManager::deleteNews(int id)
+    {
+      int index = searchNewsId(id);
+      news->erase(news->begin()+index);
+      totalNews--;
+    }
 
     /** Loads `News` from database. */
-    NewsManager();
+    NewsManager::NewsManager()
+    {
+      ifstream file("database/news.txt");
+      
+      if (!file.is_open())
+      {
+        throw "Failed to open database/news.txt";
+      }
+      
+      string line;
+      getline(file, line);  // Skip first line as it is header of the file
+      while (getline(file, line))
+      {
+        string id;
+        stringstream ss(line);
+        vector<string> fields;
+
+        // Split by ^ (the delimiter)
+        while (getline(ss, id, '^'))
+        {
+          fields.push_back(id);
+        }
+
+        news->push_back(News{
+          stoi(fields[0]),
+          stoi(fields[1]),
+          Datetime::sToDatetime(fields[2]),
+          Datetime::sToDatetime(fields[3]),
+          Datetime::sToDatetime(fields[4]),
+          fields[5],
+          fields[6],
+          stoi(fields[7]),
+          stoi(fields[8]),
+          stoi(fields[9]) == 1
+        });
+      
+        totalNews++;
+      }
+      if (totalNews > 0)
+        nextNewsId = (*news)[totalNews-1].id + 1;
+    }
 
     /** Saves `News` inside the database. */
-    void saveNews() const;
+    void NewsManager::saveNews() const
+    {
+      ofstream outNews("database/news.txt");
+
+      if (!outNews.is_open())
+        throw "Failed to open database/news.txt";
+
+      outNews << "id^publisherId^createdAt^editedAt^publishedAt^title^content^numOfLikes^numOfDislikes^isPublished\n";
+
+      for (auto it = news->begin(); it < news->end(); it++)
+        outNews << it->id << "^" << it->publisherId << "^" << Datetime::datetimeToS(it->createdAt) << "^" << Datetime::datetimeToS(it->editedAt) << "^" << Datetime::datetimeToS(it->publishedAt) << "^" << it->title << "^" << it->content << "^" << it->numOfLikes << "^" << it->numOfDislikes << "^" << it->isPublished << "\n";
+    }
 
     /** Stores `News` inside database and deletes DMA inside `News` class. */
-    ~NewsManager();
+    NewsManager::~NewsManager()
+    {
+      saveNews();
+      delete news;
+      news = nullptr;
+    }
 
     /** Returns index of `News` with specified `id`. */
-    int searchNewsId(int id) const;
+    int NewsManager::searchNewsId(int id) const
+    {
+      int index = 0;
+      for (auto it = news->begin(); it < news->end(); it++)
+      {
+        if (it->id == id)
+        {
+          return index;
+        }
+        index++;
+      }
+      throw "ID doesn't exist";
+    }
 
     /** Searches title of `News`, then return a constant vector. */
-    vector<News> searchTitle(string title) const;
+    vector<News> NewsManager::searchTitle(string title) const
+    {
+      vector<News> res;
+      for (auto it = news->begin(); it < news->end(); it++)
+      {
+        string t = toLowerCase(it->title);
+        string search = toLowerCase(title);
+
+        if (t.find(search) != string::npos)
+          res.push_back(*it);
+      }
+      return res;
+    }
 
     /** Searches news published between `dt_start` and `dt_end`. */
-    vector<News> searchDate(Datetime dt_start, Datetime dt_end) const;
+    vector<News> NewsManager::searchDate(Datetime dt_start, Datetime dt_end) const
+    {
+      vector<News> res;
+      for (auto it = news->begin(); it < news->end(); it++)
+      {
+        if (it->createdAt >= dt_start && it->createdAt <= dt_end)
+          res.push_back(*it);
+      }
+      return res;
+    }
 };
 
 #endif
