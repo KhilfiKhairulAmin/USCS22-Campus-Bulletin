4a6fa3471bcfe6f0459942a7233028a0b19c148b
KhilfiKhairulAmin <khilfikhairulamin@gmail.com>
2024-11-25 21:01:13 +0800

    Sorry, I really need to test run first before commit :'(

--- main.cpp
+++ main.cpp
@@ -1,116 +1,63 @@
-/*-------------------------------------------------------[ CREDITS ]--------------------------------------------------------------------*/
+/*------------------------------------------------[ CREDITS ]-----------------------------------------------------------------*/
 
-/**
- * @file main.cpp
- * @author Alya, Irfan, Khilfi, Rusydina
- * @brief INTEC Insider is a campus news app that brings together all wonderful stories and events around the campus in one place for everyone to see and benefit from.
- * @version 0.1
- * @date 2024-11-25
- * 
- * @copyright Copyright (c) 2024
- * 
-*/
-
-// TODO: Create all function prototypes
 // TODO: Siapkan different parts of UI and flow
 // TODO: Dominate all the other groups by introducing some absurd feature (sound effects, image, data analytics)
 
-/*----------------------------------------------------[ HEADERS & INCLUDES ]------------------------------------------------------------*/
+/*-------------------------------------------[ HEADERS & INCLUDES ]------------------------------------------------------------*/
 
-#include "headers/Database.h"
-#include <iostream>
-#include <iomanip>
-#include <csignal>
-#include <cstdlib>
-#include <conio.h>
-#include <fstream>
-#include <sstream>
-#include <algorithm>
+
+#include "headers/Database.h"  // For connecting to the database of the application
+#include <iostream>            // For handling input and output
+#include <iomanip>             // For formatting output
+#include <csignal>             // For handling special input
+#include <conio.h>             // For handling custom input (Windows only; Compiler G++ v11.0.0+)
+
+#define ENTRY 0         // MENU NUMBER 0
+#define SIGN_UP 1       // MENU NUMBER 1
+#define SIGN_IN 2       // MENU NUMBER 2
+#define MAIN_MENU 3     // MENU NUMBER 3
+#define CREATE_NEWS 4   // MENU NUMBER 4
+#define EDIT_NEWS 5     // MENU NUMBER 5
+#define DELETE_NEWS 6   // MENU NUMBER 6
+#define EDIT_PROFILE 7  // MENU NUMBER 7
+#define CALENDAR 8      // MENU NUMBER 8
+
 using namespace std;
 
-/*-----------------------------------------------------[ GLOBAL VARIABLES ]--------------------------------------------------------------*/
 
-/** MENU numbers */
-#define ENTRY 0
-#define SIGN_UP 1
-#define SIGN_IN 2
-#define MAIN_MENU 3
-#define CREATE_NEWS 4
-#define EDIT_NEWS 5
-#define DELETE_NEWS 6
-#define EDIT_PROFILE 7
-#define CALENDAR 8
-
-/** Publisher ID of current user */
-int PID = 0;
-
-/** Current Menu number the user is interacting with */
-int MENU = 0;
-
-/** Stores all publishers data */
-const vector<Publisher>* publishers;
-
-/** Stores all news data */
-const vector<News>* news;
-
-/*----------------------------------------------------[ FUNCTION PROTOTYPES ]------------------------------------------------------------*/
-
-int PublisherManager::totalPublishers = 0;
-int PublisherManager::nextPublisherId = 1;
-int NewsManager::totalNews = 0;
-int NewsManager::nextNewsId = 1;
-string Database::version = "0.0.1";
-
-string toLowerCase(const string& str);
-
-/*-----------------------------------------------------[ MAIN PROGRAM ]------------------------------------------------------------------*/
+/*--------------------------------[ GLOBAL VARIABLES & FUNCTION PROTOTYPES ]---------------------------------------------------*/
 
 
+const vector<Publisher>* publishers;  // Stores all publishers data
+const vector<News>* news;             // Stores all news data
+
+int PID = 0;   // Publisher ID of current user
+int MENU = 0;  // Current Menu number the user is interacting with
+
+void entryMenu(),     // Function prototypes for all menus
+     signUp(),
+     signIn(),
+     mainMenu(),
+     createNews(),
+     editNews(),
+     deleteNews(), 
+     editProfile(),  
+     calendar();
 
 
+/*---------------------------------------------[ MAIN PROGRAM ]----------------------------------------------------------------*/
 
 
+int main()
+{
+    Database db = Database();
+    publishers = db.getAllPublishers();
+    
+    mainMenu();
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*----------------------------------------[ FUNCTION DEFINITIONS ]-------------------------------------------------------------*/
 
 void entryMenu()
 {
@@ -129,12 +76,4 @@ void mainMenu()
 {
     Publisher p = publishers->at(PID);
     cout << p.name;
-}
-
-int main()
-{
-    Database db = Database();
-    publishers = db.getAllPublishers();
-    
-    mainMenu();
 }
